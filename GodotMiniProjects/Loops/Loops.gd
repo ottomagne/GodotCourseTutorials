69ee76f7c3419a0b26e19b0c08fb1dec43d3be15
Love Bennett <lovelylebennett@gmail.com>
2023-12-18 22:08:20 -0600

    Went through '4 Mini Projects' course

---
+++ GodotMiniProjects/Loops/Loops.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var spawn_count : int = 500
+var star_scene = preload("res://Loops/Star.tscn") 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in spawn_count:
+		var star = star_scene.instantiate()
+		add_child(star)
+		
+		star.position.x = randi_range(-280, 280)
+		star.position.y = randi_range(-150, 150)
+		
+		var star_size = randf_range(0.5, 1.0)
+		star.scale = Vector2.ONE * star_size
